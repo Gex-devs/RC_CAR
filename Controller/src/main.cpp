e28a3ec8af63b71df3dd192f8d53b2d462515567
Gex-dev69 <dgexion@outlook.com>
2023-04-18 19:34:46 +0300

    adjusted configurations

--- Controller/src/main.cpp
+++ Controller/src/main.cpp
@@ -9,8 +9,8 @@ void setup(){
   Serial.begin(9600);
   radio.begin();                                        
   radio.setChannel(5);  // Channel(0 ... 127)
-  radio.setDataRate(RF24_250KBPS);  // RF24_250KBPS, RF24_1MBPS, RF24_2MBPS)
-  radio.setPALevel(RF24_PA_MIN); //RF24_PA_MIN=-18dBm, RF24_PA_LOW=-12dBm, RF24_PA_HIGH=-6dBm, RF24_PA_MAX=0dBm
+  radio.setDataRate     (RF24_1MBPS);  // RF24_250KBPS, RF24_1MBPS, RF24_2MBPS)
+  radio.setPALevel      (RF24_PA_HIGH); //RF24_PA_MIN=-18dBm, RF24_PA_LOW=-12dBm, RF24_PA_HIGH=-6dBm, RF24_PA_MAX=0dBm
   radio.openWritingPipe (0x1234567890LL);  // 0x1234567890
 }
 void loop(){
@@ -20,3 +20,4 @@ void loop(){
   delay(1000);
   Serial.println("sent");
 }
+
--- car_driver/src/main.cpp
+++ car_driver/src/main.cpp
@@ -1,41 +1,28 @@
 #include <Arduino.h>
 #include <driver.h>
-#include <SPI.h>
-#include "RF24.h"
+#include <SPI.h>      
+#include <nRF24L01.h> 
+#include <RF24.h>     
 
-// Setup the NRF24L01+ radio module
-RF24 radio(22, 0); // CE, CSN pins
-const uint64_t pipes[2] = {0xE7E7E7E7E7, 0xC2C2C2C2C2};
-
-void setup() {
+RF24 radio(9, 10);                       
+byte data[2];
+void setup(){
+  delay(1000);
   Serial.begin(9600);
-  radio.begin();
-  radio.setRetries(15,15);
-  radio.setPayloadSize(32);
-  radio.setChannel(5);
-  radio.setDataRate(RF24_250KBPS);
-  radio.setPALevel(RF24_PA_MIN);
-  radio.setAutoAck(true);
-  radio.enableDynamicPayloads();
-  radio.openReadingPipe(1, pipes[1]);
-  radio.startListening();
+  Serial.println("Setup");
+  radio.begin();                                        
+  radio.setChannel(5);                                  // Channel  0 ... 127
+  radio.setDataRate     (RF24_1MBPS);                   // RF24_250KBPS, RF24_1MBPS, RF24_2MBPS
+  radio.setPALevel      (RF24_PA_HIGH);                 // RF24_PA_MIN=-18dBm, RF24_PA_LOW=-12dBm, RF24_PA_HIGH=-6dBm, RF24_PA_MAX=0dBm
+  radio.openReadingPipe (1, 0x1234567890LL);            // 0x1234567890
+  radio.startListening  ();    
+  Serial.println(radio.available());                         
+//radio.stopListening   ();                             
 }
-
-void loop() {
-  // Check if there is data available to read
-  if (radio.available()) {
-    uint8_t received_data[32];
-    radio.read(&received_data, sizeof(received_data));
-
-    // Print the received data
-    Serial.print("Received: ");
-    for (int i = 0; i < sizeof(received_data); i++) {
-      Serial.print((char)received_data[i]);
-    }
-    Serial.println();
-
-    // Reset the radio module to prepare for the next transmission
-    radio.stopListening();
-    radio.startListening();
+void loop(){
+  if(radio.available()){                           
+    radio.read(&data, 2);                  
+    Serial.print(data[0]);
+    Serial.println(data[1]);
   }
 }
