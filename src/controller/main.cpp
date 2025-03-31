7abbe8599306548426b68ad3137035d5fc39fe7f
¨ged¨ <gedi@gmail.com>
2025-03-31 19:33:43 +0200

    update

--- src/controller/main.cpp
+++ src/controller/main.cpp
@@ -4,40 +4,26 @@
 #include <SPI.h>
 #include <printf.h>
 
-
-RF24 radio(2, 15); //(CE, CSN)
-// RF24 radio(7, 8); //(CE, CSN)
-String data;
+RF24 radio(7, 8); // CE, CSN
+const byte addresses[][6] = {"00001", "00002"};
 void setup()
 {
   Serial.begin(9600);
   radio.begin();
-  radio.setChannel(5);                   // Channel(0 ... 127)
-  radio.setDataRate(RF24_1MBPS);         // RF24_250KBPS, RF24_1MBPS, RF24_2MBPS)
-  radio.setPALevel(RF24_PA_HIGH);        // RF24_PA_MIN=-18dBm, RF24_PA_LOW=-12dBm, RF24_PA_HIGH=-6dBm, RF24_PA_MAX=0dBm
-  radio.openWritingPipe(0x1234567890LL); // 0x1234567890
-
-
+  radio.openWritingPipe(addresses[1]);    // 00002
+  radio.openReadingPipe(1, addresses[0]); // 00001
+  radio.setPALevel(RF24_PA_MIN);
 }
 void loop()
 {
-  if (Serial.available() > 0)
-  {
-    char receivedData[64];                                            // buffer to hold the received data
-    int numBytesRead = Serial.readBytesUntil('\n', receivedData, 64); // read the data until a newline character is received or buffer is full
-    receivedData[numBytesRead] = '\0';
-    Serial.println(receivedData);
-    radio.write(&receivedData, 6);
-    // if (receivedData == "start")
-    // {
-    //   data = "start";
-    //   radio.write(&data, 6);
-    // }
-    // else if (receivedData == "stop")
-    // {
-    //   data = "stops";
-    //   radio.write(&data, 6);
-    // }
-  }
-  
+  delay(5);
+
+  radio.stopListening();
+  int potValue = analogRead(A0); // Read button instead
+  int angleValue = map(potValue, 0, 1023, 0, 180);
+  radio.write(&angleValue, sizeof(angleValue));
+  delay(5);
+  radio.startListening();
+  while (!radio.available())
+    ;
 }
--- src/driver/main.cpp
+++ src/driver/main.cpp
@@ -1,54 +1,43 @@
 #include <Arduino.h>
-#include <driver.h>
 #include <SPI.h>
-#include <nRF24L01.h>
+// #include <nRF24L01.h>
 #include <RF24.h>
 
-int motor2_A = PC3;
-int motor2_B = PC2;
+int motor_A_1 = A0;
+int motor_A_2 = A1;
 
-int motor4_A = PD3;
-int motor4_B = PD7;
-
-int motor3_A = PD4;
-int motor3_B = PD2;
 
 RF24 radio(10, 9);
-char data[6];
-Vroom yo = Vroom();
+const byte addresses[][6] = {"00001", "00002"};
 
 void setup()
 {
-  pinMode(PD0, OUTPUT);
-  delay(1000);
-  Serial.begin(9600);
-  Serial.println("Setup");
+  pinMode(motor_A_1, OUTPUT);
+  pinMode(motor_A_2, OUTPUT);
   radio.begin();
-  radio.setChannel(5);                      // Channel  0 ... 127
-  radio.setDataRate(RF24_1MBPS);            // RF24_250KBPS, RF24_1MBPS, RF24_2MBPS
-  radio.setPALevel(RF24_PA_HIGH);           // RF24_PA_MIN=-18dBm, RF24_PA_LOW=-12dBm, RF24_PA_HIGH=-6dBm, RF24_PA_MAX=0dBm
-  radio.openReadingPipe(1, 0x1234567890LL); // 0x1234567890
-  radio.startListening();
-  Serial.println(radio.available());
-  // radio.stopListening   ();
-}
-void loop()
-{
-  if (radio.available())
-  {
-    radio.read(&data, 6);
-    String dataStr(data); // convert the char array to a String object
-    Serial.println(dataStr);
-    if (dataStr == "start")
-    {
-      yo.Forward();
-      Serial.println("Moving Forward");
-    }
-    else if (dataStr == "stops")
-    {
-      yo.Stop();
-      Serial.println("Stopping");
-    }
-  }
+  radio.openWritingPipe(addresses[0]);    // 00001
+  radio.openReadingPipe(1, addresses[1]); // 00002
+  radio.setPALevel(RF24_PA_MIN);
 }
 
+void loop()
+{
+  delay(5);
+  radio.startListening();
+  if (radio.available())
+  {
+    while (radio.available())
+    {
+      int angleV = 0;
+      radio.read(&angleV, sizeof(angleV));
+      if (angleV > 0)
+      {
+        digitalWrite(motor_A_1, HIGH);
+      }else{
+        digitalWrite(motor_A_1, LOW);
+      }
+    }
+    delay(5);
+    radio.stopListening();
+  }
+}
